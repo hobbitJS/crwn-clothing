dc8627a7babbdd919395917c39cb3b849e3f1d25
hobbitJS <stanciu_vasile@mail.ru>
2019-12-01 03:16:39 +0200

    created SignInComponent, CustomButtonComponent

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+
+import "./sign-in.styles.scss";
+
+import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component";
+
+export default class SignIn extends Component {
+  state = {
+    email: "",
+    password: ""
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.setState({
+      email: "",
+      password: ""
+    });
+  };
+
+  handleChange = e => {
+    const { value, name } = e.target;
+
+    this.setState({
+      [name]: value
+    });
+  };
+
+  render() {
+    const { email, password } = this.state;
+
+    return (
+      <div className="sign-in">
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
+
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            type="email"
+            name="email"
+            value={email}
+            required
+            handleChange={this.handleChange}
+            label="Email"
+          />
+          <FormInput
+            type="password"
+            name="password"
+            value={password}
+            required
+            handleChange={this.handleChange}
+            label="Password"
+          />
+
+          <CustomButton type="submit">Sign In</CustomButton>
+        </form>
+      </div>
+    );
+  }
+}
