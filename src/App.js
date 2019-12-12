117da04b938c1a9cd4c7877cf4a497cdbf4e74c2
hobbitJS <stanciu_vasile@mail.ru>
2019-12-12 23:56:11 +0200

    finishing sing in handleSubmit method

--- src/App.js
+++ src/App.js
@@ -29,8 +29,6 @@ export default class App extends Component {
               ...snapshot.data()
             }
           });
-
-          console.log(this.state);
         });
       }
 
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -5,7 +5,7 @@ import "./sign-in.styles.scss";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
 
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 
 export default class SignIn extends Component {
   state = {
@@ -13,8 +13,17 @@ export default class SignIn extends Component {
     password: ""
   };
 
-  handleSubmit = e => {
-    e.preventDefault();
+  handleSubmit = async event => {
+    event.preventDefault();
+
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: "", password: "" });
+    } catch (error) {
+      console.error(error);
+    }
 
     this.setState({
       email: "",
