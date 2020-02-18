b6743ed64e387246394075fe10c89e03d7b6737b
hobbitJS <stanciu_vasile@mail.ru>
2020-02-19 01:14:15 +0200

    modified the method of pulling the data from firestore

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -29,7 +29,7 @@ class ShopPage extends Component {
 
     const collectionRef = firestore.collection("collections");
 
-    this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
+    collectionRef.get().then(snapshot => {
       const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
       updateCollections(collectionsMap);
       this.setState({ loading: false });
