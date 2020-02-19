c9340b04e61ecbe0141db1f506cf9634e38195bd
hobbitJS <stanciu_vasile@mail.ru>
2020-02-19 04:04:54 +0200

    implemented redux-thunk library in our project for asynchronous redux, and also created containers for collection.component and collections.overview

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,20 +1,26 @@
 import React from "react";
+import { Link, useRouteMatch } from "react-router-dom";
 
 import "./collection-preview.styles.scss";
 
 import CollectionItem from "./../collection-item/collection-item.component";
 
-const CollectionPreview = ({ title, items }) => (
-  <div className="collection-preview">
-    <h1 className="title">{title.toUpperCase()}</h1>
-    <div className="preview">
-      {items
-        .filter((item, idx) => idx < 4)
-        .map(item => (
-          <CollectionItem key={item.id} item={item} />
-        ))}
-    </div>
-  </div>
-);
+const CollectionPreview = ({ title, items }) => {
+  let match = useRouteMatch();
 
+  return (
+    <div className="collection-preview">
+      <Link to={`${match.path}/${title.toLowerCase()}`}>
+        <h1 className="title">{title.toUpperCase()}</h1>
+      </Link>
+      <div className="preview">
+        {items
+          .filter((item, idx) => idx < 4)
+          .map(item => (
+            <CollectionItem key={item.id} item={item} />
+          ))}
+      </div>
+    </div>
+  );
+};
 export default CollectionPreview;
